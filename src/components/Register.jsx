d2214867b1972a2d0bb40f800ee15d7b17bd5996
aweston <austin.inhyuk.chong@gmail.com>
2025-10-28 22:31:33 -0700

    Start Register Page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -36,41 +36,45 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Register</button>
-        <Link to="/login">Back to Login</Link>
-      </form>
-      {error && <p>{error}</p>}
-      {loading && <p>{"Registering user..."}</p>}
+    <div className="flex items-center justify-center bg-[#f1f3f4] min-h-screen">
+      <div className="bg-white w-200 h-200 flex flex-col gap-3 rounded-xl shadow-[0_1px_4px_rgba(0,0,0,0.4)] items-center justify-center">
+        <img src="/images/google.svg" alt="Google" className="w-30"/>
+        <h1 className="text-3xl">Register</h1>
+        <p>Create a "mock" Google Account</p>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Register</button>
+          <Link to="/login">Back to Login</Link>
+        </form>
+        {error && <p>{error}</p>}
+        {loading && <p>{"Registering user..."}</p>}
+      </div>
     </div>
   );
 };
